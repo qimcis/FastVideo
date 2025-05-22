bff0755546a8f12d07b3db86568565b122887cf7
Wenxuan Tan <wenxuan.tan@wisc.edu>
2025-05-22 14:37:34 -0500

    Fix version number  (#422)

--- fastvideo/__init__.py
+++ fastvideo/__init__.py
@@ -2,4 +2,6 @@ from fastvideo.v1.configs.pipelines import PipelineConfig
 from fastvideo.v1.configs.sample import SamplingParam
 from fastvideo.v1.entrypoints.video_generator import VideoGenerator
 
-__all__ = ["VideoGenerator", "PipelineConfig", "SamplingParam"]
+__version__ = "0.1.0"
+
+__all__ = ["VideoGenerator", "PipelineConfig", "SamplingParam", "__version__"]
