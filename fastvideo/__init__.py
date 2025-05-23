60295cc03f3f94dad6a08fc4749941e9c4660558
Wenxuan Tan <wtan45@wisc.edu>
2025-05-23 16:17:33 -0500

    Use version.py (#424)

--- fastvideo/__init__.py
+++ fastvideo/__init__.py
@@ -1,7 +1,6 @@
 from fastvideo.v1.configs.pipelines import PipelineConfig
 from fastvideo.v1.configs.sample import SamplingParam
 from fastvideo.v1.entrypoints.video_generator import VideoGenerator
-
-__version__ = "0.1.0"
+from fastvideo.version import __version__
 
 __all__ = ["VideoGenerator", "PipelineConfig", "SamplingParam", "__version__"]
