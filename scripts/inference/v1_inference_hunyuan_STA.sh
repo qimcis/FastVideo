a9d8699c684047b027b81983921f8effbd63bc8a
Wei Zhou <69577934+JerryZhou54@users.noreply.github.com>
2025-06-02 23:54:12 -0700

    Update v1 inference scripts (#467)
    Co-authored-by: “BrianChen1129” <yongqich@umich.edu>

--- scripts/inference/v1_inference_hunyuan_STA.sh
+++ scripts/inference/v1_inference_hunyuan_STA.sh
@@ -8,19 +8,17 @@ export MODEL_BASE=hunyuanvideo-community/HunyuanVideo
 # Note that the tp_size and sp_size should be the same and equal to the number
 # of GPUs. They are used for different parallel groups. sp_size is used for
 # dit model and tp_size is used for encoder models.
-torchrun --nnodes=1 --nproc_per_node=$num_gpus --master_port 29503 \
-    fastvideo/v1/sample/v1_fastvideo_inference.py \
-    --sp_size ${num_gpus} \
-    --tp_size ${num_gpus} \
+fastvideo generate \
+    --model-path $MODEL_BASE \
+    --sp-size ${num_gpus} \
+    --tp-size ${num_gpus} \
     --height 768 \
     --width 1280 \
-    --num_frames 117 \
-    --num_inference_steps 50 \
-    --guidance_scale 1 \
-    --embedded_cfg_scale 6 \
-    --flow_shift 7 \
-    --prompt_path ./assets/prompt.txt \
+    --num-frames 117 \
+    --num-inference-steps 50 \
+    --guidance-scale 1 \
+    --embedded-cfg-scale 6 \
+    --flow-shift 7 \
+    --prompt "A beautiful woman in a red dress walking down a street" \
     --seed 1024 \
-    --output_path outputs_video/ \
-    --model_path $MODEL_BASE \
-    --vae-sp
+    --output-path outputs_video/
--- scripts/inference/v1_inference_stepvideo.sh
+++ scripts/inference/v1_inference_stepvideo.sh
@@ -5,19 +5,18 @@ export FASTVIDEO_ATTENTION_BACKEND=
 num_gpus=2
 url='127.0.0.1'
 model_dir=data/stepvideo-t2v
-torchrun --nnodes=1 --nproc_per_node=$num_gpus --master_port 29503 \
-    fastvideo/v1/sample/v1_fastvideo_inference.py \
-    --sp_size ${num_gpus} \
-    --tp_size ${num_gpus} \
+fastvideo generate \
+    --model-path $model_dir \
+    --sp-size ${num_gpus} \
+    --tp-size ${num_gpus} \
     --height 256 \
     --width 256 \
-    --num_frames 29 \
-    --num_inference_steps 50 \
-    --embedded_cfg_scale 9.0 \
-    --guidance_scale 9.0 \
-    --prompt_path ./assets/prompt.txt \
+    --num-frames 29 \
+    --num-inference-steps 50 \
+    --embedded-cfg-scale 9.0 \
+    --guidance-scale 9.0 \
+    --prompt "A beautiful woman in a red dress walking down a street" \
     --seed 1024 \
-    --output_path outputs_stepvideo/ \
-    --model_path $model_dir \
-    --flow_shift 13.0 \
-    --vae_precision bf16
+    --output-path outputs_stepvideo/ \
+    --flow-shift 13.0 \
+    --vae-precision bf16
