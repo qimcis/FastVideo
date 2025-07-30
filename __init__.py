51711d590682bc6af9a63aef681294a24d0c17f7
Kevin Lin <42618777+kevin314@users.noreply.github.com>
2025-07-29 20:21:09 -0500

    [ComfyUI] Add __init__.py for node discovery (#663)

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from .comfyui.video_generator.nodes import (NODE_CLASS_MAPPINGS,
+                                            NODE_DISPLAY_NAME_MAPPINGS)
+
+WEB_DIRECTORY = "./web"
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
