055e52e5ea44b30cf982a3f248baa36ef386a39c
William Lin <SolitaryThinker@users.noreply.github.com>
2025-08-29 01:13:37 -0700

    [misc] [VSA] [STA] fix tk_root in setup.py for VSA and STA (#772)

--- csrc/attn/sliding_tile_attn/setup.py
+++ csrc/attn/sliding_tile_attn/setup.py
@@ -15,7 +15,7 @@ DESCRIPTION = "Sliding Tile Atteniton Kernel Used in FastVideo"
 URL = "https://github.com/hao-ai-lab/FastVideo/tree/main/csrc/sliding_tile_attention"
 
 # Set environment variables
-tk_root = os.getenv('THUNDERKITTENS_ROOT', os.path.abspath(os.path.join(os.getcwd(), '../tk/')))
+tk_root = os.getenv('THUNDERKITTENS_ROOT', os.path.abspath(os.path.join(os.getcwd(), 'tk/')))
 python_include = subprocess.check_output(['python', '-c',
                                           "import sysconfig; print(sysconfig.get_path('include'))"]).decode().strip()
 torch_include = subprocess.check_output([
