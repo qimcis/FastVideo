13d0aae706ef4d71c2f2e1b7f9bb35c354f50488
RandNMR73 <99706358+RandNMR73@users.noreply.github.com>
2025-09-24 15:11:38 -0700

    Add Sage Attention 3 Backend (#815)

--- fastvideo/platforms/interface.py
+++ fastvideo/platforms/interface.py
@@ -18,6 +18,7 @@ class AttentionBackendEnum(enum.Enum):
     SLIDING_TILE_ATTN = enum.auto()
     TORCH_SDPA = enum.auto()
     SAGE_ATTN = enum.auto()
+    SAGE_ATTN_THREE = enum.auto()
     VIDEO_SPARSE_ATTN = enum.auto()
     VMOBA_ATTN = enum.auto()
     NO_ATTENTION = enum.auto()
